7e464e07166d0a742d507ae684a422cb577a1c4e
D3press3d <anthonyrosman20@gmail.com>
2022-09-28 11:38:44 -0400

    :contruction: add hover effect in social sidebar

--- src/components/SocialBar.tsx
+++ src/components/SocialBar.tsx
@@ -1,43 +1,64 @@
 import styled from '@emotion/styled'
 import { FiGithub, FiLinkedin } from 'react-icons/fi'
 import { SiCodewars } from 'react-icons/si'
+import ReactDOM from 'react-dom'
 
 const SocialContainer = styled.div`
-    display: flex;
-    min-height: 100vh;
-    justify-content: center;
-    flex-direction: column;
-    gap: 10px;
-    position: fixed;
-    left: 2rem;
-    top: 50%;
-    transform: translateY(-50%);
+  display: flex;
+  min-height: 100vh;
+  justify-content: center;
+  flex-direction: column;
+  gap: 15px;
+  position: fixed;
+  left: 2rem;
+  top: 50%;
+  transform: translateY(-50%);
 
-    &::after,
-    &::before{
-        content: "";
-        display: block;
-        width: 1px;
-        left: 50%;
-        margin: 0 auto;
-        height: 30vh;
-        background-color: red;
+  &::after,
+  &::before {
+    content: '';
+    display: block;
+    width: 1px;
+    left: 50%;
+    margin: 0 auto;
+    height: 30vh;
+    background-color: red;
+  }
+
+  h2 {
+    transform: translateY(0);
+    transition: all 0.5s ease;
+    cursor: pointer;
+
+    &:hover {
+      transform: translateY(-10px);
     }
-`
+  }
 
+  .icon {
+    color: white;
+  }
+
+  .icon:hover {
+    color: red;
+    transition: all 0.5s ease;
+  }
+`
 const SocialBar = (): JSX.Element => {
-  return (
-    <SocialContainer>
-      <h2>
-        <FiGithub />
-      </h2>
-      <h2>
-        <FiLinkedin />
-      </h2>
-      <h2>
-        <SiCodewars />
-      </h2>
-    </SocialContainer>
+  return ReactDOM.createPortal(
+    <>
+      <SocialContainer>
+        <h2>
+          <FiGithub className='icon' />
+        </h2>
+        <h2>
+          <FiLinkedin className='icon' />
+        </h2>
+        <h2>
+          <SiCodewars className='icon' />
+        </h2>
+      </SocialContainer>
+    </>, document.getElementById('social-sidebar') as HTMLDivElement
   )
 }
 
